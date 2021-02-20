6d279566b2b12c1e65804e61bd38e2c4ab36fb7e
Ivan Diaz <ivan.diaz.pereira.alu@iesjulioverne.es>
2021-02-20 08:50:38 +0100

    commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/15/802ea2ea4f73001b14d8d0e68c196e85
@@ -0,0 +1,3 @@
+<?php
+echo str_replace ("worldl", "ñe", "Hello worldl!");
+?>
--- w3schools/switch-case.php
+++ w3schools/switch-case.php
@@ -1,3 +1,3 @@
 <?php
-echo str_replace ("worldl", "ñe", "Hello worldl!");
+echo str_replace ("worldl", "ñe", "Hello worldl!"); // indicamos la palabra que queremos sustituir + el sustituto
 ?>
